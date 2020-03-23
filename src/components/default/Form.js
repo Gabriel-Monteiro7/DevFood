74ee58fdf7d841f6782f858bf3de9ba586e65940
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2020-03-23 20:19:14 -0300

    Ajuste no select do formulario

--- src/components/default/Form.js
+++ src/components/default/Form.js
@@ -1,121 +1,101 @@
-import { Input } from "@rocketseat/unform";
-import React, { useEffect, useState } from "react";
-import { FaCaretDown, GoTrashcan } from "react-icons/all";
-import { useDispatch, useSelector } from "react-redux";
-import * as Yup from "yup";
-import api from "../../service/api";
-import {
-  addRecipeRequest,
-  deleteRecipeRequest,
-  updateRecipeRequest
-} from "../../store/modules/recipe/actions";
-import { StyleForm } from "../../styles/form";
-import swal from "sweetalert";
+import { Input } from '@rocketseat/unform';
+import React, { useEffect, useState } from 'react';
+import { FaCaretDown, GoTrashcan } from 'react-icons/all';
+import { useDispatch, useSelector } from 'react-redux';
+import * as Yup from 'yup';
+import api from '../../service/api';
+import { addRecipeRequest, deleteRecipeRequest, updateRecipeRequest } from '../../store/modules/recipe/actions';
+import { StyleForm } from '../../styles/form';
+import swal from 'sweetalert';
 let schema = Yup.object().shape({
-  title: Yup.string()
-    .max(100)
-    .required("Campo obrigatório"),
-  description: Yup.string().required("Campo obrigatório")
+	title: Yup.string()
+		.max(100)
+		.required('Campo obrigatório'),
+	description: Yup.string().required('Campo obrigatório'),
 });
 export default function Form() {
-  let { token } = useSelector(state => state.auth);
-  let { user } = useSelector(state => state.user);
-  let { selectedRecipe } = useSelector(state => state.recipe);
-  selectedRecipe = selectedRecipe === null ? {} : selectedRecipe;
-  const [category, setCategory] = useState(
-    selectedRecipe.category !== undefined ? selectedRecipe.category.id : ""
-  );
-  const [options, setOptions] = useState([]);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    async function getCategory() {
-      return await api.apiRecipe.get("category/", {
-        headers: { Authorization: "Token " + token }
-      });
-    }
-    getCategory().then(response => {
-      setOptions(
-        response.data.map(value => {
-          return { ...value, title: value.name };
-        })
-      );
-    });
-  }, [token]);
-  function handleSubmit({ title, description }, { resetForm }) {
-    let recipe = {
-      title,
-      category,
-      description,
-      user: user.id
-    };
-    if (selectedRecipe.user !== undefined) {
-      dispatch(updateRecipeRequest(token, recipe, selectedRecipe.id));
-    } else {
-      dispatch(addRecipeRequest(token, recipe));
-    }
-    resetForm();
-  }
-  function handleDelete() {
-    swal({
-      title: "Tem certeza que deseja excluir ?",
-      buttons: { false: "Cancelar", true: "excluir" }
-    }).then(willDelete => {
-      if (willDelete === "true") {
-        dispatch(deleteRecipeRequest(token, selectedRecipe));
-      }
-    });
-  }
-  return (
-    <StyleForm
-      initialData={selectedRecipe}
-      schema={schema}
-      onSubmit={handleSubmit}
-    >
-      {selectedRecipe.user !== undefined && (
-        <GoTrashcan className="btn-delete" onClick={handleDelete} />
-      )}
-      <div className="row">
-        <div className="col-12">
-          <Input name="title" type="text" placeholder={"Nome da receita"} />
-        </div>
-        <div className="col-xl-7 col-md-8 col-12">
-          <select
-            required
-            name="category"
-            value={category}
-            onChange={event => {
-              setCategory(event.target.value);
-            }}
-          >
-            <option value="" disabled hidden>
-              Escolha a categoria da receita
-            </option>
-            {options.map((category, index) => {
-              return (
-                <option value={category.id} key={index}>
-                  {category.name}
-                </option>
-              );
-            })}
-          </select>
+	let { token } = useSelector(state => state.auth);
+	let { user } = useSelector(state => state.user);
+	let { selectedRecipe } = useSelector(state => state.recipe);
+	selectedRecipe = selectedRecipe === null ? {} : selectedRecipe;
+	const [category, setCategory] = useState(selectedRecipe.category !== undefined ? selectedRecipe.category.id : '');
+	const [options, setOptions] = useState([]);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		async function getCategory() {
+			return await api.apiRecipe.get('category/', {
+				headers: { Authorization: 'Token ' + token },
+			});
+		}
+		getCategory().then(response => {
+			setOptions(
+				response.data.map(value => {
+					return { ...value, title: value.name };
+				})
+			);
+		});
+	}, [token]);
+	function handleSubmit({ title, description }, { resetForm }) {
+		let recipe = {
+			title,
+			category,
+			description,
+			user: user.id,
+		};
+		if (selectedRecipe.user !== undefined) {
+			dispatch(updateRecipeRequest(token, recipe, selectedRecipe.id));
+		} else {
+			dispatch(addRecipeRequest(token, recipe));
+		}
+		resetForm();
+	}
+	function handleDelete() {
+		swal({
+			title: 'Tem certeza que deseja excluir ?',
+			buttons: { false: 'Cancelar', true: 'excluir' },
+		}).then(willDelete => {
+			if (willDelete === 'true') {
+				dispatch(deleteRecipeRequest(token, selectedRecipe));
+			}
+		});
+	}
+	return (
+		<StyleForm initialData={selectedRecipe} schema={schema} onSubmit={handleSubmit}>
+			{selectedRecipe.user !== undefined && <GoTrashcan className="btn-delete" onClick={handleDelete} />}
+			<div className="row">
+				<div className="col-12">
+					<Input name="title" type="text" placeholder={'Nome da receita'} />
+				</div>
+				<div className="select col-xl-7 col-md-8 col-12">
+					<select
+						required
+						name="category"
+						value={category}
+						onChange={event => {
+							setCategory(event.target.value);
+						}}
+					>
+						<option value="" disabled hidden>
+							Escolha a categoria da receita
+						</option>
+						{options.map((category, index) => {
+							return (
+								<option value={category.id} key={index}>
+									{category.name}
+								</option>
+							);
+						})}
+					</select>
 
-          <FaCaretDown />
-        </div>
-        <div className="col-12">
-          <Input
-            name="description"
-            multiline
-            type="text"
-            label={"Descrição"}
-            placeholder={``}
-          />
-        </div>
-        <button type="submit">
-          {selectedRecipe.user !== undefined
-            ? "Atualizar Receita"
-            : "Criar Receita"}
-        </button>
-      </div>
-    </StyleForm>
-  );
+					<FaCaretDown />
+				</div>
+				<div className="col-12">
+					<Input name="description" multiline type="text" label={'Descrição'} placeholder={``} />
+				</div>
+				<button type="submit">
+					{selectedRecipe.user !== undefined ? 'Atualizar Receita' : 'Criar Receita'}
+				</button>
+			</div>
+		</StyleForm>
+	);
 }
--- src/styles/form.js
+++ src/styles/form.js
@@ -1,97 +1,98 @@
-import styled from "styled-components";
-import { Form } from "@rocketseat/unform";
-import { darken } from "polished";
-import { md, sm, xl } from "./global";
+import styled from 'styled-components';
+import { Form } from '@rocketseat/unform';
+import { darken } from 'polished';
+import { md, sm, xl } from './global';
 
 export const StyleForm = styled(Form)`
-  position: relative;
-  background: white !important;
-  /* border-radius: 8px; */
-  padding: 20px;
-  width: 75%;
-  @media (max-width: ${sm}px) {
-    width: 100%;
-    padding: 20px 0px;
-  }
-  margin: auto;
-  input,
-  select,
-  textarea,
-  option {
-    width: 100%;
-    outline: none;
-    font-size: 15px;
-    border: 0px;
-    padding: 10px 20px;
-    color: #6e6e6e;
-  }
-  input {
-    border: none;
-    background: #d9d9d9;
-    overflow: visible;
-    border-radius: 2px;
-    outline: none;
-    width: 100%;
-    border-radius: 40px;
-  }
-  select {
-    border-radius: 40px;
-    cursor: pointer;
-    -webkit-appearance: none;
-  }
-  textarea {
-    border-radius: 15px;
-    min-height: 130px;
-    /* cursor: default; */
-    background: #d9d9d9;
-  }
+	position: relative;
+	background: white !important;
+	/* border-radius: 8px; */
+	padding: 20px;
+	width: 75%;
+	@media (max-width: ${sm}px) {
+		width: 100%;
+		padding: 20px 0px;
+	}
+	margin: auto;
+	input,
+	select,
+	textarea {
+		width: 100%;
+		outline: none;
+		font-size: 15px;
+		border: 0px;
+		padding: 10px 20px;
+		color: #6e6e6e;
+		background: #d9d9d9;
+	}
+	input {
+		border: none;
+		overflow: visible;
+		border-radius: 2px;
+		outline: none;
+		width: 100%;
+		border-radius: 40px;
+	}
+	select {
+		border-radius: 40px;
+		cursor: pointer;
+		-webkit-appearance: none;
+		option {
+			background: white;
+		}
+	}
+	textarea {
+		border-radius: 15px;
+		min-height: 130px;
+		background: #d9d9d9;
+	}
 
-  button {
-    border: 0px;
-    padding: 12px 105px;
-    background: #f58324;
-    color: white;
-    font-size: 15px;
-    margin: 15px auto;
-    @media (max-width: ${sm}px) {
-      padding: 12px 95px;
-    }
-    &:hover {
-      background: ${darken(0.05, "#F58324")};
-    }
-  }
-  label {
-    font-size: 20px;
-    margin: 17px 0px;
-    color: #f58324;
-  }
-  div {
-    margin: 5px 0px;
-    span {
-      font-size: 12px;
-      color: #fe2e2e;
-    }
-  }
-  #id {
-    vertical-align: sub;
-  }
-  .row {
-    padding: 20px 160px;
-    @media (max-width: ${xl}px) {
-      padding: 20px 100px;
-    }
-    @media (max-width: ${md}px) {
-      padding: 20px 0px;
-    }
-  }
-  svg {
-    position: absolute;
-    right: 35px;
-    top: 13px;
-  }
-  .btn-delete {
-    right: 20px;
-    font-size: 27px;
-    cursor: pointer;
-  }
+	button {
+		border: 0px;
+		padding: 12px 105px;
+		background: #f58324;
+		color: white;
+		font-size: 15px;
+		margin: 15px auto;
+		@media (max-width: ${sm}px) {
+			padding: 12px 95px;
+		}
+		&:hover {
+			background: ${darken(0.05, '#F58324')};
+		}
+	}
+	label {
+		font-size: 20px;
+		margin: 17px 0px;
+		color: #f58324;
+	}
+	div {
+		margin: 5px 0px;
+		span {
+			font-size: 12px;
+			color: #fe2e2e;
+		}
+	}
+	#id {
+		vertical-align: sub;
+	}
+	.row {
+		padding: 20px 160px;
+		@media (max-width: ${xl}px) {
+			padding: 20px 100px;
+		}
+		@media (max-width: ${md}px) {
+			padding: 20px 0px;
+		}
+	}
+	svg {
+		position: absolute;
+		right: 35px;
+		top: 13px;
+	}
+	.btn-delete {
+		right: 20px;
+		font-size: 27px;
+		cursor: pointer;
+	}
 `;
